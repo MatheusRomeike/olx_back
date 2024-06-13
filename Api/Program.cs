c14021001a968ebd58fdee2a74578c15462e1507
Matheus <matheus_romeike@sisand.com.br>
2024-06-13 20:36:30 -0300

    OLX009-configurando-ciclos-json

--- Api/Program.cs
+++ Api/Program.cs
@@ -39,7 +39,10 @@ ConfigureServices(builder.Services);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>

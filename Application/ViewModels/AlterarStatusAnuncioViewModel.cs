178533f688c47aa5867c6dace01bfa2158b1ddfc
Matheus <matheus_romeike@sisand.com.br>
2024-06-13 22:56:47 -0300

    correções-master

---
+++ Application/ViewModels/AlterarStatusAnuncioViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Anuncio.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels
+{
+    public class AlterarStatusAnuncioViewModel
+    {
+        public int AnuncioId { get; set; }
+        public EstadoAnuncio Estado { get; set; }
+    }
+}
